c40e73abae84eae909eed9f47bcfb8e839b5c572
vtran-at-speer <viet.tran@speer.io>
2023-07-01 17:02:20 -0400

    Implemented JWT verificaiton.

---
+++ src/main/java/com/viettran/security/config/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.viettran.security.config;
+
+import com.viettran.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
